01679b5a4054396ae645499bee3f3effed4c90c3
YonnieHicks <85805865+YonnieHicks@users.noreply.github.com>
2023-03-23 14:01:38 -0500

    TestTaskThree Completed.

--- src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
@@ -5,42 +5,35 @@ import org.launchcode.techjobs.mvc.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 
 import static org.launchcode.techjobs.mvc.controllers.ListController.columnChoices;
 
-
-/**
- * Created by LaunchCode
- */
 @Controller
-@RequestMapping("search")
 public class SearchController {
 
-    @GetMapping(value = "")
+    @GetMapping(value = "search")
     public String search(Model model) {
         model.addAttribute("columns", columnChoices);
         return "search";
     }
 
-    // TODO #3 - Create a handler to process a search request and render the updated search view.
+    @PostMapping(value = "results")
+    public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
+        ArrayList<Job> jobs;
 
-    @GetMapping(value = "results")
-    public String searchResults(Model model, @RequestParam String searchTerm, @RequestParam String searchType) {
-        ArrayList<Job> searchResults;
-        if (searchType.equals("all")) {
-            searchResults = JobData.findByValue(searchTerm);
+        if (searchTerm.toLowerCase().equals("all") || searchTerm.isEmpty()) {
+            jobs = JobData.findAll();
         } else {
-            searchResults = JobData.findByColumnAndValue(searchType, searchTerm);
+            jobs = JobData.findByColumnAndValue(searchType, searchTerm);
         }
-        model.addAttribute("searchResults", searchResults);
+
+        model.addAttribute("jobs", jobs);
         model.addAttribute("columns", columnChoices);
+
         return "search";
     }
-
-
 }
