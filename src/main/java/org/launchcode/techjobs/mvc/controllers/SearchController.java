9e63edccdff048a77730f1f71fa549485e48e83d
YonnieHicks <85805865+YonnieHicks@users.noreply.github.com>
2023-03-26 20:43:56 -0500

    19 of 20 Test pass!

--- src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
@@ -12,22 +12,18 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.util.ArrayList;
 
 import static org.launchcode.techjobs.mvc.controllers.ListController.columnChoices;
-/**
- * Created by LaunchCode
- */
 
 @Controller
 @RequestMapping(value = "")
 public class SearchController {
 
-    @GetMapping(value = "search")
+    @GetMapping(value = "/search")
     public String search(Model model) {
         model.addAttribute("columns", columnChoices);
         return "search";
     }
 
-    // TODO #3 - Create a handler to process a search request and render the updated search view.
-    @PostMapping(value = "results")
+    @PostMapping(value = "/search/results")
     public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
         ArrayList<Job> jobs;
 
